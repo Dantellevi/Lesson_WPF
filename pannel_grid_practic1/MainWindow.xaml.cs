36723ae32c0eda3d00a0665277ac0701bf7465ed
Alex <dante.l.levi93@gmail.com>
2017-09-15 13:05:22 +0400

    Work_width_event_pannel

--- pannel_grid_practic1/MainWindow.xaml.cs
+++ pannel_grid_practic1/MainWindow.xaml.cs
@@ -40,21 +40,51 @@ namespace pannel_grid_practic1
 
         }
 
+        //------------------------------------------------------------------------
+                //Показывает панель 1 когда указатель мыши находится над ее кнопкой
         private void panel1Button_MouseEnter(object sender, MouseEventArgs e)
         {
+            layer1.Visibility = Visibility.Visible;
+            //коррекцируем Z-порядок , чтобы панель всегда была вверху:
+            Grid.SetZIndex(layer1, 1);
+            Grid.SetZIndex(layer2, 0);
+            //скрываем вторую панел если она не пристыкована
+            if(panel2Button.Visibility==Visibility.Visible)
+            {
+                layer2.Visibility = Visibility.Collapsed;
+            }
 
         }
 
+        //-----------------------------------------------------------------------
+        //Показываем панель 2 когда указатель мыши находится над ее кнопкой
         private void panel2Button_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            layer2.Visibility = Visibility.Visible;
+            //Коррекцируем Z-порядок, чтобы панел всегда была вверху:
+            Grid.SetZIndex(layer2, 1);
+            Grid.SetZIndex(layer1, 0);
+            //скрываем вторую панель если она не пристыкована
+            if(panel1Button.Visibility==Visibility.Visible)
+            {
+                layer1.Visibility = Visibility.Collapsed;
+            }
         }
+
+        //------------------------------------------------------------------------
 
         private void layer0_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            if(panel1Button.Visibility==Visibility.Visible)
+            {
+                layer1.Visibility = Visibility.Collapsed;
+            }
+            if(panel2Button.Visibility==Visibility.Visible)
+            {
+                layer2.Visibility = Visibility.Collapsed;
+            }
         }
-
+        //-------------------------------------------------------------------------
         private void Grid_MouseEnter(object sender, MouseEventArgs e)
         {
 
@@ -64,6 +94,34 @@ namespace pannel_grid_practic1
         {
 
         }
+        //-------------------------------------------------------------
+        //пристыковать панель, при этом скрывается соответствующая ей кнопка
+        public void DockPane(int paneNumber)
+        {
+            if(paneNumber==1)
+            {
+                panel1Button.Visibility = Visibility.Collapsed;
+                panel1PinImage.Source = new BitmapImage(new Uri("bin/Debug/pin.gif", UriKind.Relative));
+                //добавляем клонированный столбец в сой 0:
+                layer0.ColumnDefinitions.Add(column1CloneForLayer0);
+                //добавляем клонированный столбец в слой 1, но только если панель 2 пристыкована
+                if(panel2Button.Visibility==Visibility.Collapsed)
+                {
+                    layer1.ColumnDefinitions.Add(column2CloneForLayer1);
+                }
+                else if(paneNumber==2)
+                {
+                    pane2PinImage.Source= new BitmapImage(new Uri("bin/Debug/pin.gif", UriKind.Relative));
+                    //добавляем клонированный столбец в слой 0
+                    layer0.ColumnDefinitions.Add(column2CloneForLayer0);
+                    //добавляем клонированный столбец с лой 1, но только если панель 1 пристыкована
+                    if(panel1Button.Visibility==Visibility.Collapsed)
+                    {
+                        layer1.ColumnDefinitions.Add(column2CloneForLayer1);
+                    }
+                }
+            }
+        }
         //--------------------------------------------------------------
         public void UndockPane(int paneNumber)
         {
@@ -72,7 +130,7 @@ namespace pannel_grid_practic1
                 layer1.Visibility = Visibility.Visible;
                 panel1Button.Visibility = Visibility.Visible;
                 panel1PinImage.Source = new BitmapImage(
-                    new Uri("pinHorizontal.gif", UriKind.Relative));
+                    new Uri("bin/Debug/pinHorizontal.gif", UriKind.Relative));
                 //Удаляем клонированные столюцы из слоев 0 и 1:
                 layer0.ColumnDefinitions.Remove(column1CloneForLayer0);
                 //Этот столбец присутствует  не всегда , но метод remove
@@ -83,7 +141,7 @@ namespace pannel_grid_practic1
             {
                 layer2.Visibility = Visibility.Visible;
                 panel2Button.Visibility = Visibility.Visible;
-                pane2PinImage.Source = new BitmapImage(new Uri("pinHorizontal.gif", UriKind.Relative));
+                pane2PinImage.Source = new BitmapImage(new Uri("bin/Debug/pinHorizontal.gif", UriKind.Relative));
                 //удаляем клонированные столбцы из слоев 0 и 1:
                 layer0.ColumnDefinitions.Remove(column2CloneForLayer0);
                 layer1.ColumnDefinitions.Remove(column2CloneForLayer1);
@@ -102,6 +160,11 @@ namespace pannel_grid_practic1
             {
                 UndockPane(1);
             }
+            else
+            {
+                DockPane(1);
+            }
+
         }
 
         private void Grid_MouseEnter_2(object sender, MouseEventArgs e)
@@ -111,17 +174,39 @@ namespace pannel_grid_practic1
 
         private void panel2Pin_Click(object sender, RoutedEventArgs e)
         {
-
+            if(panel2Button.Visibility==Visibility.Collapsed)
+            {
+                UndockPane(2);
+            }
+            else
+            {
+                DockPane(2);
+            }
         }
 
+
+        //-------------------------------------------------------------------
+        //скрываем вторую панель если оона не пристыкована, когда указатель мыши перемещается
+        //на панель 1
         private void Grid_MouseEnter_3(object sender, MouseEventArgs e)
         {
-
+            //скрываем вторую панель если она не пристыкована
+            if(panel2Button.Visibility==Visibility.Visible)
+            {
+                layer2.Visibility = Visibility.Collapsed;
+            }
         }
+
+        //--------------------------------------------------------------------
+        //Скрываем вторую панель если она не пристыкована, когда указатель мыши перемещается на панель2
 
         private void Grid_MouseEnter_4(object sender, MouseEventArgs e)
         {
-
+            //скрываем вторую панель если она не пристыкована
+            if(panel1Button.Visibility==Visibility.Visible)
+            {
+                layer1.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
