1e9aae18a84a4755b48dfbe7592c0ef9e08b72b7
Alex <dante.l.levi93@gmail.com>
2017-09-21 10:39:52 +0400

    Практика WPF.Создание приложения Фотогалереи. Вариант разметки приложения.

---
+++ WindowsPhotoGallery/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsPhotoGallery
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void favoritesMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void renameMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _Refresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E_xit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fixMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void printMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void treeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+        }
+
+        private void pictureBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zoomPopup_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void zoomSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void defaultSizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void previousButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void slideshowButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void counterclockwiseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clockwiseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void zoomButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
